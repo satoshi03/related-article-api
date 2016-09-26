e9d9e4e5bb64f618c820586f570f2eb86b82f3c0
satoshi03 <innamisatoshi@gmail.com>
2016-09-27 07:20:17 +0900

    add url helper

---
+++ utils/url.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func NormalizeURL(s string) (string, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+
+	path := u.Path
+	if strings.HasSuffix(path, "/") {
+		path = path[:len(path)-1]
+	}
+	return fmt.Sprintf("%s%s", u.Host, u.Path), nil
+}
+
+func ToMd5Hex(s string) string {
+	md5sum := md5.Sum([]byte(s))
+	return hex.EncodeToString(md5sum[:])
+}
